39210fcb76c9f0030661f9c8ebfec6ba0b8430cd
solomon <sorael11@gmail.com>
2019-01-07 12:53:48 -0500

    first

---
+++ APIDemoWithGit/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace APIDemoWithGit.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ APIDemoWithGit/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace APIDemoWithGit.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
