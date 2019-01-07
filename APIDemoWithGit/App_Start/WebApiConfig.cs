dfb9a1e24a595336256f3147d82b903716e0be9f
solomon <sorael11@gmail.com>
2019-01-07 13:43:01 -0500

    second commit

--- APIDemoWithGit/App_Start/WebApiConfig.cs
+++ APIDemoWithGit/App_Start/WebApiConfig.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
+//using WebApiContrib.Formatting.Jsonp;
 
 namespace APIDemoWithGit
 {
@@ -9,16 +12,16 @@ namespace APIDemoWithGit
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
 
-            // Web API routes
-            config.MapHttpAttributeRoutes();
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            //EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*");
+            //config.EnableCors(cors);
+
+            //var jsonpFormatter = new JsonpMediaTypeFormatter(config.Formatters.JsonFormatter);
+            //config.Formatters.Insert(0, jsonpFormatter);
         }
     }
 }
---
+++ APIDemoWithGit/EmployeeSecurity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EmployeeDataAccess;
+
+namespace APIDemoWithGit
+{
+    public class EmployeeSecurity
+    {
+        public static bool Login(string username, string password)
+        {
+            using (EmployeeDBEntities db = new EmployeeDBEntities())
+            {
+                return db.Users.Any(user => user.Username.Equals(username, StringComparison.OrdinalIgnoreCase) && user.Password == password);
+            }
+        }
+    }
+}
